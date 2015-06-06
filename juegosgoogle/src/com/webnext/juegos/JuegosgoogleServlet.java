d676d8f152a479ff2c787805b001d19809e331b3
manu <manu@manu-portatil2>
2015-06-07 00:10:17 +0200

    Añadido aula115mfd

---
+++ juegosgoogle/src/com/webnext/juegos/JuegosgoogleServlet.java
@@ -0,0 +1,11 @@
+package com.webnext.juegos;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class JuegosgoogleServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
